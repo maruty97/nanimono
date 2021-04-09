fea7e59ac19064f241af1d15bba498d248c1371a
こうちゃんのmac <kouchannomac@kouchannomacnoMacBook-Pro.local>
2021-04-09 15:57:04 +0900

    nanimonohtml

---
+++ resources/views/nanimono_home.blade.php
@@ -0,0 +1,50 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>ナニモノ　ホーム</title>
+        <link rel="stylesheet" href="{{ secure_asset('/css/nanimono.css') }}">
+        <script type="text/javascript" src=></script>
+    </head>
+
+    <body>
+        <div id="navi">
+            <ul>
+                <li><a href="#home">ホーム</a></li>
+                <li><a href="#explanation">サービス概要</a></li>
+                <li><a href="#recommond">どんな人におすすめ</a></li>
+                <li><a href="nanimono_signUp.html">新規登録</a></li>
+                <li><a href="nanimono_login.html">ログイン</a></li>
+            </ul>
+
+        </div>
+
+        <div id="home">
+            <h1>ナニモノ</h1>
+
+        </div>
+
+        <div id="explanation">
+            <h1>ナニモノとは？</h1>
+            <p>説明文が入ります。</p>
+
+        </div>
+
+        <div id="recommond">
+            <h1>どんな人におすすめ</h1>
+            <ul>
+                <li>学生に</li>
+                <li>日記や自己分析に</li>
+                <li>日報や人材育成に</li>
+            </ul>
+
+        </div>
+                
+    
+    </body>
+
+
+    <footer>
+
+    </footer>
+
+</html>
